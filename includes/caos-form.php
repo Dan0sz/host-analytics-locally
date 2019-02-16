28e78b5a97c38937cd67a72f048a3f9f5a09d673
DaanvandenBergh <DaanvandenBergh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2019-02-16 10:27:00 +0000

    v2.1.0: Added status bar for displaying cron and file health
    git-svn-id: http://plugins.svn.wordpress.org/host-analyticsjs-local/trunk@2032015 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/caos-form.php
+++ includes/caos-form.php
@@ -1,16 +1,35 @@
 <?php
+/**
+ * @author: Daan van den Bergh
+ * @url: https://dev.daanvandenbergh.com/wordpress-plugins/optimize-analytics-wordpress/
+ * @copyright: (c) 2019 Daan van den Bergh
+ * @license: GPL2v2 or later
+ */
 
-?>
+$fileStatus = caos_analytics_cron_status();
+
+if ($fileStatus): ?>
+<div id="setting-error-settings_updated" class="updated settings-success notice">
+    <p><strong><?php _e('Your cron is running healthy.', 'host-analyticsjs-local'); ?></strong></p>
+    <p><em>analytics.js</em> <?php _e('last updated at', 'host-analyticsjs-local'); ?>: <?php echo date('Y-m-d H:i:s', caos_analytics_last_updated()); ?></p>
+    <p><?php _e('Next update scheduled at', 'host-analytics-local'); ?>: <?php echo date('Y-m-d H:i:s', caos_analytics_cron_next_scheduled()); ?></p>
+</div>
+<?php else: ?>
+<div id="setting-error-settings_updated" class="notice notice-error">
+    <p><strong><?php _e('analytics.js hasn\'t been updated for more than two days. Is your cron running?', 'host-analyticsjs-local'); ?></strong></p>
+</div>
+<?php endif; ?>
+
 <div class="caos_left_column" style="float:left; width: 50%;">
     <h3><?php _e('Basic Settings'); ?></h3>
     <table class="form-table">
         <tr valign="top">
             <th scope="row"><?php _e( 'Google Analytics Tracking ID', 'host-analyticsjs-local' ); ?></th>
             <td>
-                <input type="text" name="sgal_tracking_id" value="<?php echo CAOS_TRACKING_ID; ?>"/>
+                <input type="text" name="sgal_tracking_id" value="<?php echo CAOS_ANALYTICS_TRACKING_ID; ?>"/>
             </td>
         </tr>
-        <tbody class="caos_basic_settings" <?php echo CAOS_MI_COMPATIBILITY == 'on' ? 'style="display: none;"' : ''; ?>>
+        <tbody class="caos_basic_settings" <?php echo CAOS_ANALYTICS_MI_COMPATIBILITY == 'on' ? 'style="display: none;"' : ''; ?>>
         <tr valign="top">
             <th scope="row"><?php _e( 'Allow tracking...', 'host-analyticsjs-local' ); ?></th>
             <td>
@@ -25,7 +44,7 @@
                     <label>
                         <input type="radio" class="caos_allow_tracking_<?php echo $option; ?>"
                                name="caos_allow_tracking" value="<?php echo $option; ?>"
-							<?php echo $option == CAOS_ALLOW_TRACKING ? 'checked="checked"' : ''; ?>/>
+							<?php echo $option == CAOS_ANALYTICS_ALLOW_TRACKING ? 'checked="checked"' : ''; ?>/>
 						<?php echo _e( $label, 'host-analyticsjs-local'); ?>
                     </label>
                     <br/>
@@ -41,22 +60,22 @@
             </td>
         </tr>
         <tr class="caos_gdpr_setting caos_allow_tracking_name"
-            valign="top" <?php echo CAOS_ALLOW_TRACKING ? '' : 'style="display: none;"'; ?>>
+            valign="top" <?php echo CAOS_ANALYTICS_ALLOW_TRACKING ? '' : 'style="display: none;"'; ?>>
             <th scope="row"><?php _e( 'Cookie name', 'host-analyticsjs-local'); ?></th>
             <td>
                 <input type="text" name="sgal_cookie_notice_name"
-                       value="<?php echo CAOS_COOKIE_NAME; ?>"/>
+                       value="<?php echo CAOS_ANALYTICS_COOKIE_NAME; ?>"/>
                 <p class="description">
                     <?php _e('The cookie name set by your Cookie Notice plugin when user accepts.', 'host-analyticsjs-local'); ?>
                 </p>
             </td>
         </tr>
         <tr class="caos_gdpr_setting caos_allow_tracking_name caos_allow_tracking_value"
-            valign="top" <?php echo CAOS_ALLOW_TRACKING == 'cookie_has_value' ? '' : 'style="display: none;"'; ?>>
+            valign="top" <?php echo CAOS_ANALYTICS_ALLOW_TRACKING == 'cookie_has_value' ? '' : 'style="display: none;"'; ?>>
             <th scope="row"><?php _e( 'Cookie value', 'host-analyticsjs-local' ); ?></th>
             <td>
                 <input type="text" name="caos_cookie_value"
-                       value="<?php echo CAOS_COOKIE_VALUE; ?>"/>
+                       value="<?php echo CAOS_ANALYTICS_COOKIE_VALUE; ?>"/>
                 <p class="description">
                     <?php _e('The value of the above specified cookie set by your Cookie Notice when user accepts.', 'host-analyticsjs-local'); ?>
                 </p>
@@ -75,7 +94,7 @@
 				foreach ( $sgal_script_position as $option => $label ): ?>
                     <label>
                         <input class="caos_script_position_<?php echo $option; ?>" type="radio" name="sgal_script_position"
-                               value="<?php echo $option; ?>" <?php echo $option == CAOS_SCRIPT_POSITION ? 'checked="checked"' : ''; ?> />
+                               value="<?php echo $option; ?>" <?php echo $option == CAOS_ANALYTICS_SCRIPT_POSITION ? 'checked="checked"' : ''; ?> />
                         <?php echo _e($label, 'host-analyticsjs-local'); ?>
                     </label>
                     <br/>
@@ -86,7 +105,7 @@
                 </p>
             </td>
         </tr>
-        <tr class="caos_add_manually" valign="top" <?php echo CAOS_SCRIPT_POSITION == 'manual' ? '' : 'style="display: none;"'; ?>>
+        <tr class="caos_add_manually" valign="top" <?php echo CAOS_ANALYTICS_SCRIPT_POSITION == 'manual' ? '' : 'style="display: none;"'; ?>>
             <th scope="row"><?php _e('Tracking-code', 'host-analyticsjs-local'); ?></th>
             <td>
                 <label>
@@ -108,7 +127,7 @@
             <tr valign="top">
                 <th scope="row"><?php _e('Enable compatibility with Monster Insights?', 'host-analyticsjs-local'); ?></th>
                 <td>
-                    <input class="caos_mi_compatibility_checkbox" type="checkbox" name="caos_mi_compatibility" <?php echo CAOS_MI_COMPATIBILITY == 'on' ? 'checked = "checked"' : ''; ?> />
+                    <input class="caos_mi_compatibility_checkbox" type="checkbox" name="caos_mi_compatibility" <?php echo CAOS_ANALYTICS_MI_COMPATIBILITY == 'on' ? 'checked = "checked"' : ''; ?> />
                     <p class="description">
 	                    <?php _e('The best choice, if you want to use enhanced Analytics features, such as event tracking in e.g. WooCommerce.', 'host-analyticsjs-local'); ?>
 	                    <?php _e('Allow Monster Insights\' plugin to use the locally hosted analytics.js-file generated and updated by CAOS. Enabling this option means that you\'ll manage Google Analytics entirely within Google Analytics by Monster Insights.', 'host-analyticsjs-local'); ?>
@@ -126,12 +145,12 @@
                 </td>
             </tr>
         </tbody>
-        <tbody class="caos_advanced_settings" <?php echo CAOS_MI_COMPATIBILITY == 'on' ? 'style="display: none;"' : ''; ?>>
+        <tbody class="caos_advanced_settings" <?php echo CAOS_ANALYTICS_MI_COMPATIBILITY == 'on' ? 'style="display: none;"' : ''; ?>>
         <tr valign="top">
             <th scope="row"><?php _e( 'Cookie expiry period (days)', 'host-analyticsjs-local' ); ?></th>
             <td>
                 <input type="number" name="sgal_ga_cookie_expiry_days" min="0" max="365"
-                       value="<?php echo CAOS_COOKIE_EXPIRY; ?>" />
+                       value="<?php echo CAOS_ANALYTICS_COOKIE_EXPIRY; ?>" />
                 <p class="description">
                     <?php _e('The number of days when the cookie will automatically expire.', 'host-analyticsjs-local'); ?>
                 </p>
@@ -141,7 +160,7 @@
             <th scope="row"><?php _e( 'Use adjusted bounce rate?', 'host-analyticsjs-local' ); ?></th>
             <td>
                 <input type="number" name="sgal_adjusted_bounce_rate" min="0" max="60"
-                       value="<?php echo CAOS_ADJUSTED_BOUNCE_RATE; ?>" />
+                       value="<?php echo CAOS_ANALYTICS_ADJUSTED_BOUNCE_RATE; ?>" />
                 <p class="description">
                     <a href="https://moz.com/blog/adjusted-bounce-rate" target="_blank"><?php _e('More information about adjusted bounce rate', 'host-analyticsjs-local'); ?></a>.
                 </p>
@@ -151,7 +170,7 @@
             <th scope="row"><?php _e( 'Change enqueue order? (Default = 0)', 'host-analyticsjs-local' ); ?></th>
             <td>
                 <input type="number" name="sgal_enqueue_order" min="0"
-                       value="<?php echo CAOS_ENQUEUE_ORDER; ?>" />
+                       value="<?php echo CAOS_ANALYTICS_ENQUEUE_ORDER; ?>" />
                 <p class="description">
                     <?php _e('Leave this alone if you don\'t know what you\'re doing.', 'host-analyticsjs-local'); ?>
                 </p>
@@ -161,7 +180,7 @@
             <th scope="row"><?php _e( 'Disable all display features functionality?', 'host-analyticsjs-local' ); ?></th>
             <td>
                 <input type="checkbox" name="caos_disable_display_features"
-                        <?php echo CAOS_DISABLE_DISPLAY_FEAT == "on" ? 'checked = "checked"' : ''; ?> />
+                        <?php echo CAOS_ANALYTICS_DISABLE_DISPLAY_FEAT == "on" ? 'checked = "checked"' : ''; ?> />
                 <p class="description">
                     <a href="https://developers.google.com/analytics/devguides/collection/analyticsjs/display-features" target="_blank"><?php _e('More information about display features', 'host-analyticsjs-local'); ?></a>.
                 </p>
@@ -171,7 +190,7 @@
             <th scope="row"><?php _e( 'Use Anonymize IP?', 'host-analyticsjs-local' ); ?></th>
             <td>
                 <input type="checkbox" name="sgal_anonymize_ip"
-                    <?php echo CAOS_ANONYMIZE_IP == "on" ? 'checked = "checked"' : ''; ?> />
+                    <?php echo CAOS_ANALYTICS_ANONYMIZE_IP == "on" ? 'checked = "checked"' : ''; ?> />
                 <p class="description">
                     <?php _e('Required by law in some countries.'); ?>
                     <a href="https://support.google.com/analytics/answer/2763052?hl=en" target="_blank"><?php _e('More information about IP Anonymization'); ?></a>.
@@ -182,7 +201,7 @@
             <th scope="row"><?php _e( 'Track logged in Administrators?', 'host-analyticsjs-local' ); ?></th>
             <td>
                 <input type="checkbox" name="sgal_track_admin"
-                    <?php echo CAOS_TRACK_ADMIN == "on" ? 'checked = "checked"' : ''; ?> />
+                    <?php echo CAOS_ANALYTICS_TRACK_ADMIN == "on" ? 'checked = "checked"' : ''; ?> />
                 <p class="description">
                     <strong><?php _e('Warning!'); ?></strong> <?php _e('This will track all your traffic as a logged in user.'); ?>
                 </p>
@@ -194,7 +213,7 @@
             <th scope="row"><?php _e( 'Remove script from wp-cron?', 'host-analyticsjs-local' ); ?></th>
             <td>
                 <input type="checkbox" name="caos_remove_wp_cron"
-                    <?php echo CAOS_REMOVE_WP_CRON == "on" ? 'checked = "checked"' : ''; ?> />
+                    <?php echo CAOS_ANALYTICS_REMOVE_CRON == "on" ? 'checked = "checked"' : ''; ?> />
                 <p class="description">
 	                <?php _e('If your local-ga.js isn\'t updated automatically or you have WP-Cron disabled, check this option and add the update-analytics.php-script to your crontab manually.', 'host-analyticsjs-local'); ?>
                 </p>
