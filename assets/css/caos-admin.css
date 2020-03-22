13072102b537e4cca74f29dcf2e38a6cdfa887e4
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2020-03-22 21:19:26 +0100

    Updated copyright notice.

--- assets/css/caos-admin.css
+++ assets/css/caos-admin.css
@@ -8,7 +8,7 @@
  *
  * @author   : Daan van den Bergh
  * @url      : https://daan.dev/wordpress-plugins/optimize-analytics-wordpress/
- * @copyright: (c) 2019 Daan van den Bergh
+ * @copyright: (c) 2020 Daan van den Bergh
  * @license  : GPL2v2 or later
  * * * * * * * * * * * * * * * * * * * */
 
