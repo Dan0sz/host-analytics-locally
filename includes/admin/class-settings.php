71fb7563a0a8df560a6fd4ffbbe662f8b4766b66
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2022-01-24 17:34:45 +0000

    Fix: not clear "two" made only works in Stealth Mode.

--- includes/admin/class-settings.php
+++ includes/admin/class-settings.php
@@ -46,7 +46,7 @@ class CAOS_Admin_Settings extends CAOS_Admin
     const CAOS_ADMIN_ANONYMIZE_IP_MODE_OPTIONS = [
         ''    => 'Off (default)',
         'one' => 'One octet',
-        'two' => 'Two octets (Pro)'
+        'two' => 'Two octets (Pro) (only works in Stealth Mode)'
     ];
     const CAOS_ADMIN_SCRIPT_POSITION_OPTIONS = [
         'header' => 'Header (default)',
