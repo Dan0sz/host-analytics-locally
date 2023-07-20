939a647019f38723f891b939063b5b4f5348d74b
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2023-07-20 12:01:06 +0000

    Added filter: caos_file_alias

--- includes/class-file-manager.php
+++ includes/class-file-manager.php
@@ -23,11 +23,6 @@ class CAOS_FileManager {
 	private $file_contents;
 
 	/**
-	 * @var string
-	 */
-	protected $plugin_text_domain = 'host-analyticsjs-local';
-
-	/**
 	 * Downloads $remoteFile, check if $localFile exists and if so deletes it, then writes it to $localFile
 	 *
 	 * @param $localFile
@@ -42,35 +37,34 @@ class CAOS_FileManager {
 		$this->file_contents = wp_remote_get( $remote_file );
 
 		if ( is_wp_error( $this->file_contents ) ) {
-			CAOS::debug( sprintf( __( 'An error occurred: %1$s - %2$s', $this->plugin_text_domain ), $this->file_contents->get_error_code(), $this->file_contents->get_error_message() ) );
+			CAOS::debug( sprintf( __( 'An error occurred: %1$s - %2$s', 'host-analyticsjs-local' ), $this->file_contents->get_error_code(), $this->file_contents->get_error_message() ) );
 
 			return $this->file_contents->get_error_code() . ': ' . $this->file_contents->get_error_message();
 		}
 
 		/**
-		 * If $file is not set, extract it from $remote_file, unless we're downloading a plugin.
+		 * If $file is not set, extract it from $remote_file.
 		 *
 		 * @since 3.11.0
 		 * @since 4.0.3 Don't rename plugins.
+		 * @since 4.7.0 Plugins no longer exist.
 		 */
 		$file         = $file ? $file : pathinfo( $remote_file )['filename'];
 		$file_aliases = CAOS::get_file_aliases();
-		$file_alias   = $file_aliases[ $file ] ?? '';
+		$file_alias   = apply_filters( 'caos_file_alias', $file_aliases[ $file ] ?? '' );
 
 		/**
-		 * If no file alias has been set (yet) and we're not downloading a plugin, generate a new alias.
+		 * If $file_alias equals 'gtag' then something has gone wrong previously while generating an alias. Let's try again.
 		 *
-		 * @since 4.0.2
-		 * @since 4.0.3 Don't rename plugins
-		 *
+		 * @since v4.7.3
 		 */
-		if ( ! $file_alias || $file_alias === 'gtag' ) {
+		if ( empty( $file_alias ) || $file_alias === 'gtag' ) {
 			$file_alias = bin2hex( random_bytes( 4 ) ) . '.js';
 		}
 
 		$local_dir = CAOS::get_local_dir();
 
-		CAOS::debug( sprintf( __( 'Saving to %s.', $this->plugin_text_domain ), $local_dir ) );
+		CAOS::debug( sprintf( __( 'Saving to %s.', 'host-analyticsjs-local' ), $local_dir ) );
 
 		/**
 		 * Some servers don't do a full overwrite if file already exists, so we delete it first.
@@ -79,12 +73,12 @@ class CAOS_FileManager {
 			$deleted = unlink( $local_dir . $file_alias );
 
 			if ( $deleted ) {
-				CAOS::debug( sprintf( __( 'File %s successfully deleted.', $this->plugin_text_domain ), $file_alias ) );
+				CAOS::debug( sprintf( __( 'File %s successfully deleted.', 'host-analyticsjs-local' ), $file_alias ) );
 			} else {
 				if ( $error = error_get_last() ) {
-					CAOS::debug( sprintf( __( 'File %1$s could not be deleted. Something went wrong: %2$s', $this->plugin_text_domain ), $file_alias, $error['message'] ) );
+					CAOS::debug( sprintf( __( 'File %1$s could not be deleted. Something went wrong: %2$s', 'host-analyticsjs-local' ), $file_alias, $error['message'] ) );
 				} else {
-					CAOS::debug( sprintf( __( 'File %s could not be deleted. An unknown error occurred.', $this->plugin_text_domain ), $file_alias ) );
+					CAOS::debug( sprintf( __( 'File %s could not be deleted. An unknown error occurred.', 'host-analyticsjs-local' ), $file_alias ) );
 				}
 			}
 		}
@@ -92,20 +86,17 @@ class CAOS_FileManager {
 		$write = file_put_contents( $local_dir . $file_alias, $this->file_contents['body'] );
 
 		if ( $write ) {
-			CAOS::debug( sprintf( __( 'File %s successfully saved.', $this->plugin_text_domain ), $file_alias ) );
+			CAOS::debug( sprintf( __( 'File %s successfully saved.', 'host-analyticsjs-local' ), $file_alias ) );
 		} else {
 			if ( $error = error_get_last() ) {
-				CAOS::debug( sprintf( __( 'File %1$s could not be saved. Something went wrong: %2$s', $this->plugin_text_domain ), $file_alias, $error['message'] ) );
+				CAOS::debug( sprintf( __( 'File %1$s could not be saved. Something went wrong: %2$s', 'host-analyticsjs-local' ), $file_alias, $error['message'] ) );
 			} else {
-				CAOS::debug( sprintf( __( 'File %s could not be saved. An unknown error occurred.', $this->plugin_text_domain ), $file_alias ) );
+				CAOS::debug( sprintf( __( 'File %s could not be saved. An unknown error occurred.', 'host-analyticsjs-local' ), $file_alias ) );
 			}
 		}
 
 		/**
-		 * Update the file alias in temporary storage, for later use. The child download() method writes the values
-		 * to the database.
-		 *
-		 * @see CAOS_Cron_Script::download()
+		 * Update the file alias in the global variable AND database.
 		 */
 		CAOS::set_file_alias( $file_alias, true );
 
@@ -136,7 +127,7 @@ class CAOS_FileManager {
 	 * @param $replace array|string
 	 */
 	public function find_replace_in( $file, $find, $replace ) {
-		CAOS::debug( sprintf( __( 'Replacing %1$s with %2$s in %3$s.', $this->plugin_text_domain ), print_r( $find, true ), print_r( $replace, true ), $file ) );
+		CAOS::debug( sprintf( __( 'Replacing %1$s with %2$s in %3$s.', 'host-analyticsjs-local' ), print_r( $find, true ), print_r( $replace, true ), $file ) );
 
 		return file_put_contents( $file, str_replace( $find, $replace, file_get_contents( $file ) ) );
 	}
