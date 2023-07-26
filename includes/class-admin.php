5713d4d5c6ace0eb229333983ec1c2fd7237d9b7
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2023-07-26 18:48:51 +0000

    Added: CAOS now checks if updates for CAOS Pro (if installed) are failing and notifies the user via the native Plugin updates screen.

--- includes/class-admin.php
+++ includes/class-admin.php
@@ -13,6 +13,8 @@
  * @license  : GPL2v2 or later
  * * * * * * * * * * * * * * * * * * * */
 
+use WpOrg\Requests\Exception\InvalidArgument;
+
 defined( 'ABSPATH' ) || exit;
 
 class CAOS_Admin {
@@ -35,6 +37,10 @@ class CAOS_Admin {
 		$this->do_advanced_settings();
 		$this->do_extensions_settings();
 		$this->do_help_section();
+
+		// Plugin Updates
+		add_action( 'all_plugins', [ $this, 'maybe_display_premium_update_notice' ] );
+		add_filter( 'wp_get_update_data', [ $this, 'maybe_add_update_count' ], 10, 1 );
 
 		// Notices
 		add_action( 'update_option_' . CAOS_Admin_Settings::CAOS_BASIC_SETTING_MEASUREMENT_ID, [ $this, 'add_tracking_code_notice' ], 10, 2 );
@@ -76,6 +82,158 @@ class CAOS_Admin {
 	 */
 	private function do_help_section() {
 		return new CAOS_Admin_Settings_Help();
+	}
+
+	/**
+	 * This function checks if:
+	 * - CAOS Pro is installed,
+	 * - And if so, if an update is already available for it.
+	 * - And if not, if the current version is lower than the latest available version.
+	 * - And if so, display a custom notice with instructions to download the update manually.
+	 *
+	 * CAOS Pro's internal ID is 3940.
+	 *
+	 * @param mixed $installed_plugins
+	 *
+	 * @return mixed
+	 */
+	public function maybe_display_premium_update_notice( $installed_plugins ) {
+		$plugin_slugs = array_keys( $installed_plugins );
+
+		/**
+		 * If CAOS Pro isn't installed, there's no need to continue.
+		 */
+		if ( ! in_array( 'caos-pro/caos-pro.php', $plugin_slugs ) ) {
+			return $installed_plugins;
+		}
+
+		if ( $this->update_already_displayed() ) {
+			return $installed_plugins;
+		}
+
+		$latest_version  = $this->get_latest_version();
+		$current_version = get_plugin_data( WP_PLUGIN_DIR . '/caos-pro/caos-pro.php' )['Version'] ?? '';
+
+		/**
+		 * If current version is lower than latest available version, take necessary measures to display notice.
+		 */
+		if ( version_compare( $current_version, $latest_version, '<' ) ) {
+			$installed_plugins['caos-pro/caos-pro.php']['update'] = true;
+
+			add_action( 'after_plugin_row_caos-pro/caos-pro.php', [ $this, 'display_premium_update_notice' ], 10, 3 );
+		}
+
+		return $installed_plugins;
+	}
+
+	/**
+	 * Checks if there's already an update available for CAOS Pro in the Plugins screen.
+	 *
+	 * @return mixed
+	 */
+	private function update_already_displayed() {
+		$available_updates = $this->get_available_updates();
+
+		if ( ! is_object( $available_updates ) ) {
+			return false;
+		}
+
+		$plugin_slugs = array_keys( $available_updates->response );
+
+		return in_array( 'caos-pro/caos-pro.php', $plugin_slugs );
+	}
+
+	/**
+	 * Fetch available updates from database.
+	 *
+	 * @return mixed
+	 */
+	private function get_available_updates() {
+		static $available_updates;
+
+		if ( $available_updates === null ) {
+			$available_updates = get_site_transient( 'update_plugins' );
+		}
+
+		return $available_updates;
+	}
+
+	private function get_latest_version() {
+		$latest_version = get_transient( 'caos_pro_latest_available_version' );
+
+		/**
+		 * If $latest_version is an empty string, that probably means something went wrong before. So,
+		 * we should try and refresh it.
+		 */
+		if ( $latest_version === false || $latest_version === '' ) {
+			$response       = wp_remote_get( 'https://daan.dev/?edd_action=get_version&item_id=3940' );
+			$latest_version = json_decode( wp_remote_retrieve_body( $response ) )->new_version ?? '';
+
+			set_transient( 'caos_pro_latest_available_version', $latest_version, DAY_IN_SECONDS );
+		}
+
+		return $latest_version;
+	}
+
+	/**
+	 * Display a notice if current version of CAOS Pro is outdated, but updates can't be retrieved.
+	 *
+	 * @action after_plugin_row_caos-pro/caos-pro.php
+	 *
+	 * @param mixed $file
+	 * @param mixed $plugin_data
+	 * @param mixed $status
+	 *
+	 * @return void
+	 */
+	public function display_premium_update_notice( $file, $plugin_data, $status ) {
+		$slug   = $plugin_data['slug'];
+		$notice = sprintf( __( 'An update for CAOS Pro is available, but we\'re having trouble retrieving it. <a href=\'%s\' target=\'_blank\'>Download it from your account area</a> and install it manually.', 'host-analyticsjs-local' ), 'https://daan.dev/docs/pre-sales/download-files/' );
+
+		/**
+		 * This snippet of JS either overwrites or appends to the contents of the update message.
+		 */
+		?>
+		<script>
+			var row = document.getElementById('<?php echo esc_attr( $slug ); ?>-update');
+			row.getElementsByTagName('p')[0].innerHTML = "<?php echo wp_kses( $notice, 'post' ); ?>";
+		</script>
+		<?php
+	}
+
+	/**
+	 * This function check if:
+	 * - CAOS Pro is installed,
+	 * - And if so, if an update is already fetched and displayed by WP itself.
+	 * - And if so, if the currently installed version is lower than the latest available version,
+	 * - And if so, adds 1 to the little update nag next to "Plugins" in the sidebar to attract
+	 *   attention to the fact that updates seem to be failing.
+	 *
+	 * @param mixed $update_data
+	 * @param mixed $plugins
+	 *
+	 * @return mixed
+	 *
+	 * @throws InvalidArgument
+	 */
+	public function maybe_add_update_count( $update_data ) {
+		if ( isset( $_GET['plugin_status'] ) && $_GET['plugin_status'] === 'upgrade' ) {
+			return $update_data;
+		}
+
+		if ( $this->update_already_displayed() ) {
+			return $update_data;
+		}
+
+		$latest_version  = $this->get_latest_version();
+		$plugin_data     = get_plugin_data( WP_PLUGIN_DIR . '/caos-pro/caos-pro.php' );
+		$current_version = $plugin_data['Version'] ?? '';
+
+		if ( version_compare( $current_version, $latest_version, '<' ) ) {
+			$update_data['counts']['plugins']++;
+		}
+
+		return $update_data;
 	}
 
 	/**
