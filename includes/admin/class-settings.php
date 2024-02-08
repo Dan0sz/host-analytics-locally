b6d97975cbc2ad5da6e28b9def239636793a364c
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2024-02-08 14:05:37 +0100

    Removed useless constant.

--- includes/admin/class-settings.php
+++ includes/admin/class-settings.php
@@ -14,19 +14,24 @@ defined( 'ABSPATH' ) || exit;
  * @copyright: © 2021 - 2024 Daan van den Bergh
  * @license  : GPL2v2 or later
  * * * * * * * * * * * * * * * * * * * */
-class CAOS_Admin_Settings extends CAOS_Admin {
 
+class CAOS_Admin_Settings extends CAOS_Admin {
 	const CAOS_ADMIN_PAGE = 'host_analyticsjs_local';
+
 	const CAOS_NEWS_REEL  = 'caos_news_reel';
+
 	const CAOS_DB_VERSION = 'caos_db_version';
 
 	/**
 	 * Admin Sections
 	 */
-	const CAOS_ADMIN_SECTION_BASIC_SETTINGS = 'caos-basic-settings';
-	const CAOS_ADMIN_SECTION_ADV_SETTINGS   = 'caos-advanced-settings';
-	const CAOS_ADMIN_SECTION_EXT_SETTINGS   = 'caos-extensions-settings';
-	const CAOS_ADMIN_SECTION_HELP           = 'caos-help';
+	const CAOS_ADMIN_SECTION_BASIC_SETTINGS  = 'caos-basic-settings';
+
+	const CAOS_ADMIN_SECTION_ADV_SETTINGS    = 'caos-advanced-settings';
+
+	const CAOS_ADMIN_SECTION_EXT_SETTINGS    = 'caos-extensions-settings';
+
+	const CAOS_ADMIN_SECTION_HELP            = 'caos-help';
 
 	const CAOS_ADMIN_ALLOW_TRACKING_OPTIONS  = [
 		''                      => 'Always (default)',
@@ -36,10 +41,12 @@ class CAOS_Admin_Settings extends CAOS_Admin {
 		'cookie_value_contains' => 'When cookie value contains (loose comparison)',
 		'consent_mode'          => 'When Consent Mode\'s <code>analytics_storage</code> property is updated to "granted" by another plugin',
 	];
+
 	const CAOS_ADMIN_TRACKING_CODE_OPTIONS   = [
 		''            => 'Asynchronous (default)',
 		'minimal_ga4' => 'Minimal Analytics 4',
 	];
+
 	const CAOS_ADMIN_SCRIPT_POSITION_OPTIONS = [
 		'header' => 'Header (default)',
 		'footer' => 'Footer',
@@ -50,29 +57,43 @@ class CAOS_Admin_Settings extends CAOS_Admin {
 	 * CAOS Basic/Advanced Settings
 	 */
 	const CAOS_BASIC_SETTING_MEASUREMENT_ID     = 'measurement_id';
+
 	const CAOS_BASIC_SETTING_TRACK_ADMIN        = 'track_admin';
+
 	const CAOS_BASIC_SETTING_ALLOW_TRACKING     = 'allow_tracking';
+
 	const CAOS_BASIC_SETTING_COOKIE_NOTICE_NAME = 'cookie_notice_name';
+
 	const CAOS_BASIC_SETTING_COOKIE_VALUE       = 'cookie_value';
+
 	const CAOS_BASIC_SETTING_TRACKING_CODE      = 'tracking_code';
+
 	const CAOS_BASIC_SETTING_SCRIPT_POSITION    = 'script_position';
+
 	const CAOS_ADV_SETTING_COMPATIBILITY_MODE   = 'compatibility_mode';
+
 	const CAOS_ADV_SETTING_CACHE_DIR            = 'analytics_cache_dir';
+
 	const CAOS_ADV_SETTING_CDN_URL              = 'analytics_cdn_url';
+
 	const CAOS_ADV_SETTING_DISABLE_ADS_FEATURES = 'disable_display_features';
+
 	const CAOS_ADV_SETTING_UNINSTALL_SETTINGS   = 'analytics_uninstall_settings';
 
 	/**
 	 * CAOS Cron
 	 */
 	const CAOS_CRON_RUN_UPDATE   = 'caos_cron_run_update';
+
 	const CAOS_CRON_FILE_ALIASES = 'caos_cron_file_aliases';
 
 	/**
 	 * Info URLs
 	 */
 	const DAAN_DEV_WORDPRESS_CAOS_PRO            = 'https://daan.dev/wordpress/caos-pro/';
+
 	const CAOS_ADMIN_SETTINGS_EXTENSIONS_TAB_URI = 'options-general.php?page=host_analyticsjs_local&tab=caos-extensions-settings';
+
 	const CAOS_SETTINGS_UTM_PARAMS_SUPPORT_TAB   = '?utm_source=caos&utm_medium=plugin&utm_campaign=support_tab';
 
 	/**
@@ -89,8 +110,8 @@ class CAOS_Admin_Settings extends CAOS_Admin {
 	 * CAOS_Admin_Settings constructor.
 	 */
 	public function __construct() {
-		$this->active_tab = isset( $_GET['tab'] ) ? $_GET['tab'] : self::CAOS_ADMIN_SECTION_BASIC_SETTINGS;
-		$this->page       = isset( $_GET['page'] ) ? $_GET['page'] : '';
+		$this->active_tab = isset( $_GET[ 'tab' ] ) ? $_GET[ 'tab' ] : self::CAOS_ADMIN_SECTION_BASIC_SETTINGS;
+		$this->page       = isset( $_GET[ 'page' ] ) ? $_GET[ 'page' ] : '';
 
 		parent::__construct();
 
@@ -122,6 +143,14 @@ class CAOS_Admin_Settings extends CAOS_Admin {
 	}
 
 	/**
+	 * Checks downloaded file and cron health.
+	 * @return CAOS_Admin_Functions
+	 */
+	private function do_cron_check() {
+		return new CAOS_Admin_Functions();
+	}
+
+	/**
 	 * Create WP menu-item
 	 */
 	public function create_menu() {
@@ -144,26 +173,36 @@ class CAOS_Admin_Settings extends CAOS_Admin {
 			wp_die( __( "You're not cool enough to access this page.", 'host-analyticsjs-local' ) );
 		} ?>
 
-		<div class="wrap caos">
-			<h1><?php _e( 'CAOS | Complete Analytics Optimization Suite', 'host-analyticsjs-local' ); ?></h1>
+        <div class="wrap caos">
+            <h1><?php _e( 'CAOS | Complete Analytics Optimization Suite', 'host-analyticsjs-local' ); ?></h1>
 
 			<?php if ( empty( CAOS::get( self::CAOS_BASIC_SETTING_TRACKING_CODE ) ) ) : ?>
-				<div class="notice notice-info">
-					<p><?php echo sprintf( __( '<strong>Gtag.js</strong> is renamed to <strong>%1$s</strong> and was last updated on <em>%2$s</em>. The next automatic update by cron is scheduled <em>%3$s</em>.', 'host-analyticsjs-local' ), CAOS::get_file_alias(), $this->file_last_updated(), $this->cron_next_scheduled() ); ?> <a id="caos-regenerate-alias" data-nonce="<?php echo wp_create_nonce( self::CAOS_ADMIN_PAGE ); ?>" title="<?php echo __( 'This will regenerate alias(es) and all files. Could be useful when running into (browser) caching issues.', 'host-analyticsjs-local' ); ?>" href="#"><?php echo __( 'Regenerate Alias(es)', 'host-analyticsjs-local' ); ?></a>.</p>
-				</div>
+                <div class="notice notice-info">
+                    <p><?php echo sprintf(
+							__(
+								'<strong>Gtag.js</strong> is renamed to <strong>%1$s</strong> and was last updated on <em>%2$s</em>. The next automatic update by cron is scheduled <em>%3$s</em>.',
+								'host-analyticsjs-local'
+							),
+							CAOS::get_file_alias(),
+							$this->file_last_updated(),
+							$this->cron_next_scheduled()
+						); ?> <a id="caos-regenerate-alias" data-nonce="<?php echo wp_create_nonce( self::CAOS_ADMIN_PAGE ); ?>" title="<?php echo __(
+							'This will regenerate alias(es) and all files. Could be useful when running into (browser) caching issues.',
+							'host-analyticsjs-local'
+						); ?>" href="#"><?php echo __( 'Regenerate Alias(es)', 'host-analyticsjs-local' ); ?></a>.</p>
+                </div>
 			<?php endif; ?>
 
-			<h2 class="caos-nav nav-tab-wrapper">
+            <h2 class="caos-nav nav-tab-wrapper">
 				<?php do_action( 'caos_settings_tab' ); ?>
-			</h2>
+            </h2>
 
-			<form id="<?php echo $this->active_tab; ?>-form" method="post" action="options.php?tab=<?php echo $this->active_tab; ?>">
+            <form id="<?php echo $this->active_tab; ?>-form" method="post" action="options.php?tab=<?php echo $this->active_tab; ?>">
 				<?php
 				ob_start();
 				settings_fields( $this->active_tab );
 				/**
 				 * We use a custom update action, so we can group all settings in one DB row upon form submit.
-				 *
 				 * @see \CAOS\Plugin update_options()
 				 */
 				$settings_fields = ob_get_clean();
@@ -182,38 +221,14 @@ class CAOS_Admin_Settings extends CAOS_Admin {
 				<?php if ( $this->active_tab !== self::CAOS_ADMIN_SECTION_HELP ) : ?>
 					<?php submit_button( __( 'Save Changes & Update', 'host-analyticsjs-local' ), 'primary', 'submit', false ); ?>
 				<?php endif; ?>
-			</form>
-		</div>
+            </form>
+        </div>
 		<?php
 	}
 
 	/**
-	 * Register all settings.
-	 *
-	 * @throws ReflectionException
-	 */
-	public function register_settings() {
-		if ( $this->active_tab !== self::CAOS_ADMIN_SECTION_BASIC_SETTINGS
-			&& $this->active_tab !== self::CAOS_ADMIN_SECTION_ADV_SETTINGS
-			&& $this->active_tab !== self::CAOS_ADMIN_SECTION_EXT_SETTINGS
-			&& $this->active_tab !== self::CAOS_ADMIN_SECTION_HELP
-		) {
-			$this->active_tab = self::CAOS_ADMIN_SECTION_BASIC_SETTINGS;
-		}
-
-		foreach ( $this->get_settings() as $constant => $value ) {
-			register_setting(
-				$this->active_tab,
-				$value
-			);
-		}
-	}
-
-	/**
 	 * Format timestamp of remote JS files' alias last updated time.
-	 *
 	 * @since v4.4.6 Explicity check if file exists.
-	 *
 	 * @return string
 	 */
 	private function file_last_updated() {
@@ -226,21 +241,6 @@ class CAOS_Admin_Settings extends CAOS_Admin {
 		$file_mod_time = filemtime( $file_alias_path );
 
 		return $this->format_time_by_locale( $file_mod_time, get_locale() );
-	}
-
-	/**
-	 * Get formatted timestamp of next scheduled cronjob.
-	 *
-	 * @return string
-	 */
-	private function cron_next_scheduled() {
-		$next_scheduled = wp_next_scheduled( CAOS_CRON );
-
-		if ( ! $next_scheduled ) {
-			return __( 'Never. Your WP cron might not be functioning properly', 'host-analyticsjs-local' );
-		}
-
-		return $this->format_time_by_locale( $next_scheduled, get_locale() );
 	}
 
 	/**
@@ -275,8 +275,41 @@ class CAOS_Admin_Settings extends CAOS_Admin {
 	}
 
 	/**
+	 * Get formatted timestamp of next scheduled cronjob.
+	 * @return string
+	 */
+	private function cron_next_scheduled() {
+		$next_scheduled = wp_next_scheduled( Caos_Setup::CRON_LABEL );
+
+		if ( ! $next_scheduled ) {
+			return __( 'Never. Your WP cron might not be functioning properly', 'host-analyticsjs-local' );
+		}
+
+		return $this->format_time_by_locale( $next_scheduled, get_locale() );
+	}
+
+	/**
+	 * Register all settings.
+	 * @throws ReflectionException
+	 */
+	public function register_settings() {
+		if ( $this->active_tab !== self::CAOS_ADMIN_SECTION_BASIC_SETTINGS &&
+			$this->active_tab !== self::CAOS_ADMIN_SECTION_ADV_SETTINGS &&
+			$this->active_tab !== self::CAOS_ADMIN_SECTION_EXT_SETTINGS &&
+			$this->active_tab !== self::CAOS_ADMIN_SECTION_HELP ) {
+			$this->active_tab = self::CAOS_ADMIN_SECTION_BASIC_SETTINGS;
+		}
+
+		foreach ( $this->get_settings() as $constant => $value ) {
+			register_setting(
+				$this->active_tab,
+				$value
+			);
+		}
+	}
+
+	/**
 	 * Get all settings for the current section using the constants in this class.
-	 *
 	 * @return array
 	 * @throws ReflectionException
 	 */
@@ -309,7 +342,6 @@ class CAOS_Admin_Settings extends CAOS_Admin {
 
 	/**
 	 * We add the assets directly to the head to avoid ad blockers blocking the URLs cause they include 'analytics'.
-	 *
 	 * @return void
 	 */
 	public function enqueue_admin_assets() {
@@ -329,10 +361,44 @@ class CAOS_Admin_Settings extends CAOS_Admin {
 	}
 
 	/**
+	 * @param      $id
+	 * @param null $icon
+	 * @param null $label
+	 */
+	private function generate_tab( $id, $icon = null, $label = null, $disabled = false ) {
+		?>
+        <a class="nav-tab dashicons-before <?php echo $icon; ?> <?php echo $this->active_tab == $id ? 'nav-tab-active' : ''; ?> <?php echo $disabled ?
+			'disabled' : ''; ?>"
+			<?php
+			if ( ! $disabled ) :
+				?>
+                href="<?php echo $this->generate_tab_link( $id ); ?>" <?php endif; ?>
+           title="<?php echo $disabled ? __( 'Advanced Settings are disabled, because Minimal Analytics is enabled.', 'host-analyticsjs-local' ) :
+	           ''; ?>">
+			<?php echo $label; ?>
+        </a>
+		<?php
+	}
+
+	/**
+	 * @param $tab
+	 *
+	 * @return string
+	 */
+	private function generate_tab_link( $tab ) {
+		return admin_url( "options-general.php?page=host_analyticsjs_local&tab=$tab" );
+	}
+
+	/**
 	 * Add Advanced Settings Tab to Settings Screen.
 	 */
 	public function do_advanced_settings_tab() {
-		$this->generate_tab( self::CAOS_ADMIN_SECTION_ADV_SETTINGS, 'dashicons-admin-settings', __( 'Advanced Settings', 'host-analyticsjs-local' ), CAOS::get( self::CAOS_BASIC_SETTING_TRACKING_CODE ) === 'minimal_ga4' );
+		$this->generate_tab(
+			self::CAOS_ADMIN_SECTION_ADV_SETTINGS,
+			'dashicons-admin-settings',
+			__( 'Advanced Settings', 'host-analyticsjs-local' ),
+			CAOS::get( self::CAOS_BASIC_SETTING_TRACKING_CODE ) === 'minimal_ga4'
+		);
 	}
 
 	/**
@@ -344,38 +410,10 @@ class CAOS_Admin_Settings extends CAOS_Admin {
 
 	/**
 	 * Add Help tab to Settings Screen.
-	 *
 	 * @return void
 	 */
 	public function do_help_tab() {
 		$this->generate_tab( self::CAOS_ADMIN_SECTION_HELP, 'dashicons-editor-help', __( 'Help', 'host-analyticsjs-local' ) );
-	}
-
-	/**
-	 * @param $id
-	 * @param null $icon
-	 * @param null $label
-	 */
-	private function generate_tab( $id, $icon = null, $label = null, $disabled = false ) {
-		?>
-		<a class="nav-tab dashicons-before <?php echo $icon; ?> <?php echo $this->active_tab == $id ? 'nav-tab-active' : ''; ?> <?php echo $disabled ? 'disabled' : ''; ?>" 
-			<?php
-			if ( ! $disabled ) :
-				?>
-				href="<?php echo $this->generate_tab_link( $id ); ?>" <?php endif; ?>
-		title="<?php echo $disabled ? __( 'Advanced Settings are disabled, because Minimal Analytics is enabled.', 'host-analyticsjs-local' ) : ''; ?>">
-			<?php echo $label; ?>
-		</a>
-		<?php
-	}
-
-	/**
-	 * @param $tab
-	 *
-	 * @return string
-	 */
-	private function generate_tab_link( $tab ) {
-		return admin_url( "options-general.php?page=host_analyticsjs_local&tab=$tab" );
 	}
 
 	/**
@@ -402,7 +440,6 @@ class CAOS_Admin_Settings extends CAOS_Admin {
 
 	/**
 	 * Changes footer text.
-	 *
 	 * @return string
 	 */
 	public function footer_text_left() {
@@ -413,10 +450,10 @@ class CAOS_Admin_Settings extends CAOS_Admin {
 
 	/**
 	 * All logic to generate the news reel in the bottom right of the footer on all of CAOS' settings pages.
-	 *
 	 * Includes multiple checks to make sure the reel is only shown if a recent post is available.
 	 *
-	 * @param  mixed $text
+	 * @param mixed $text
+	 *
 	 * @return mixed
 	 */
 	public function footer_text_right( $text ) {
@@ -466,32 +503,28 @@ class CAOS_Admin_Settings extends CAOS_Admin {
 			return $text;
 		}
 
-		$text  = sprintf( __( 'Recently tagged <a target="_blank" href="%s"><strong>#CAOS</strong></a> on my blog:', 'host-analyticsjs-local' ), 'https://daan.dev/blog/tag/caos' ) . ' ';
+		$text = sprintf(
+				__( 'Recently tagged <a target="_blank" href="%s"><strong>#CAOS</strong></a> on my blog:', 'host-analyticsjs-local' ),
+				'https://daan.dev/blog/tag/caos'
+			) . ' ';
 		$text .= '<span id="caos-ticker-wrap">';
-		$i     = 0;
+		$i    = 0;
 
 		foreach ( $items as $item ) {
 			if ( $i > 4 ) {
 				break;
 			}
 
-			$hide  = $i > 0 ? 'style="display: none;"' : '';
-			$text .= "<span class='ticker-item' $hide>" . sprintf( '<a target="_blank" href="%s"><em>%s</em></a>', $item->link, $item->title ) . '</span>';
-			$i++;
+			$hide = $i > 0 ? 'style="display: none;"' : '';
+			$text .= "<span class='ticker-item' $hide>" .
+				sprintf( '<a target="_blank" href="%s"><em>%s</em></a>', $item->link, $item->title ) .
+				'</span>';
+			$i ++;
 		}
 
 		$text .= '</span>';
 
 		return $text;
-	}
-
-	/**
-	 * Checks downloaded file and cron health.
-	 *
-	 * @return CAOS_Admin_Functions
-	 */
-	private function do_cron_check() {
-		return new CAOS_Admin_Functions();
 	}
 
 	/**
