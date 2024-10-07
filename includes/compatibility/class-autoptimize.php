693bac66b199972d0eca1dbc25162fd536987d72
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2024-10-07 20:47:29 +0200

    Added: Autoptimize compatibility for Minimal Analytics.

--- includes/compatibility/class-autoptimize.php
+++ includes/compatibility/class-autoptimize.php
@@ -13,7 +13,7 @@
  * @license  : GPL2v2 or later
  * * * * * * * * * * * * * * * * * * * */
 
-class CAOS_Compatibility {
+class CAOS_Compatibility_Autoptimize {
 	/**
 	 * Build class.
 	 */
@@ -22,21 +22,22 @@ class CAOS_Compatibility {
 	}
 
 	/**
-	 * Trigger compatibility fixes.
+	 * Action and filter hooks.
 	 *
 	 * @return void
 	 */
 	private function init() {
-		if ( defined( 'AUTOPTIMIZE_PLUGIN_VERSION' ) ) {
-			new CAOS_Compatibility_Autoptimize();
-		}
+		add_filter( 'caos_script_custom_attributes', [ $this, 'exclude_from_autoptimize' ] );
+	}
 
-		if ( defined( 'LSCWP_V' ) ) {
-			new CAOS_Compatibility_Litespeed();
-		}
-
-		if ( defined( 'WP_ROCKET_VERSION' ) ) {
-			new CAOS_Compatibility_WpRocket();
-		}
+	/**
+	 * Add data-no-optimize="1" attribute to script if LiteSpeed Cache is enabled.
+	 *
+	 * @param $attributes
+	 *
+	 * @return string
+	 */
+	public function exclude_from_autoptimize( $attributes ) {
+		return 'data-noptimize="true" ' . $attributes;
 	}
 }
